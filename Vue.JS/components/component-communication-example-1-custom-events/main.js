5856082f370242d32d083bd8cf40b67a322327bb
Viral Solani <viral.solani@gmail.com>
2018-01-17 11:19:54 +0530

    Component Communication Example 1 Custom Events

---
+++ Vue.JS/components/component-communication-example-1-custom-events/main.js
@@ -0,0 +1,25 @@
+Vue.component('coupon', {
+    template : '<input placehoder="Enter your coupon code" @blur="onCouponApplied">',
+
+     methods: {
+        onCouponApplied() {
+            this.$emit('applied');
+        }
+    }
+
+
+});
+
+new Vue({
+    el : '#root',
+
+    data: {
+        couponApplied: false
+    },
+
+    methods: {
+        onCouponApplied() {
+            this.couponApplied = true;
+        }
+    }
+});
