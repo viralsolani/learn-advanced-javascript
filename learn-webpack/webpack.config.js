e19d5c1f8418eb08349cb05e83d6cb05cd0f92cd
Viral Solani <viral.solani@gmail.com>
2018-08-03 00:32:26 +0530

    webpack configuration

--- learn-webpack/webpack.config.js
+++ learn-webpack/webpack.config.js
@@ -1,8 +1,10 @@
+const path = require("path");
+
 module.exports = {
   entry: ["./src/js/index.js"],
   output: {
-    path: __dirname,
+    path: path.resolve(__dirname, "dist/js"),
     filename: "bundle.js"
   },
-  mode: development
+  mode: "development"
 };
